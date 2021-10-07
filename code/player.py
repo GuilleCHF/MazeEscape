0761fe0016d504fcb67fa39c142955a55ec50c33
GuilleCHF <jordipilongo@hotmail.com>
2021-10-07 14:19:39 +0200

    folders added

--- code/player.py
+++ code/player.py
@@ -5,6 +5,7 @@ from settings import *
 class Player(pygame.sprite.Sprite):
     def __init__(self) -> None:
         super().__init__()
+
         self.image = pygame.Surface((PLAYER_SIZE, PLAYER_SIZE))
         self.image.fill("blue")
         pos = (SCREEN_SIZE//2, SCREEN_SIZE//2)
---
+++ code/support.py
@@ -0,0 +1,6 @@
+import pygame
+import os
+
+
+def get_images(path) -> list[pygame.Surface]:
+    pass
